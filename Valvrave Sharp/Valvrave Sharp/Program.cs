c1a63c09ddc8a1e0368471f5c7be914ea2300757
NightMoon <1076751236@qq.com>
2015-12-20 20:41:59 +0800

    Fix OrbwalkerMode => Orbwalking & OrbwalkerType => OrbwalkingType

--- Valvrave Sharp/Valvrave Sharp/Program.cs
+++ Valvrave Sharp/Valvrave Sharp/Program.cs
@@ -69,7 +69,7 @@
             MainMenu.Separator("Paypal: dcbrian01@gmail.com");
             Orbwalker.Init(MainMenu);
             Plugins[Player.ChampionName].Invoke();
-            LeagueSharp.SDK.Core.Orbwalker.Enabled = false;
+            //LeagueSharp.SDK.Core.Orbwalking.Enabled = false;
         }
 
         private static void InitSummonerSpell()

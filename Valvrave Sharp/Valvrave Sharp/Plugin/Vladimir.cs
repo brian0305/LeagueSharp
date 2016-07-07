9f393dd7c38c3c77484fe2e92e7ecfffd0b90226
Brian Nguyen <nemo3596@gmail.com>
2016-07-07 10:59:27 +0700

    a

---
+++ Valvrave Sharp/Valvrave Sharp/Plugin/Vladimir.cs
@@ -0,0 +1,31 @@
+﻿namespace Valvrave_Sharp.Plugin
+{
+    #region
+
+    using LeagueSharp;
+    using LeagueSharp.SDK;
+    using LeagueSharp.SDK.Enumerations;
+
+    #endregion
+
+    internal class Vladimir : Program
+    {
+        #region Constructors and Destructors
+
+        public Vladimir()
+        {
+            Q = new Spell(SpellSlot.Q, 600).SetTargetted(0.25f, float.MaxValue);
+            W = new Spell(SpellSlot.W, 150);
+            E = new Spell(SpellSlot.E, 600);
+            R = new Spell(SpellSlot.R, 700).SetSkillshot(
+                0.25f,
+                175,
+                float.MaxValue,
+                false,
+                SkillshotType.SkillshotCircle);
+            Q.DamageType = W.DamageType = E.DamageType = R.DamageType = DamageType.Magical;
+        }
+
+        #endregion
+    }
+}
